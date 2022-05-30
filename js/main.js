8a98ffc7feffb1aac892aaf668e43e726f8e8a86
SohelTanbir <soheltanbir19@gmail.com>
2022-05-30 17:59:30 +0600

    add a function to toggle icon of sidebar to show sub menu item

--- js/main.js
+++ js/main.js
@@ -4,8 +4,21 @@ $(document).ready(function(){
 
 // toggle sub menu
 function toggleSubMenu(){
-    const subMenu = this.children[1];
-    subMenu.classList.toggle("show")
+    const subMenu = this?.children[1];
+
+    if(subMenu){
+     subMenu.classList.toggle("show");
+    }
+    toggleIcon(this);
+}
+
+// toggle sub menu icon
+function toggleIcon(targetElement){
+    if(targetElement.children[0].children[0].children[0]){
+       targetElement.children[0].children[0].children[0].classList.toggle("hide");
+       targetElement.children[0].children[0].children[1].classList.toggle("show");
+    }
+    
 }
 
 
