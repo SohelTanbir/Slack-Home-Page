725ba14f19b287c3ecc6e52dfdccb8f4908b01ad
SohelTanbir <soheltanbir19@gmail.com>
2022-05-31 10:00:26 +0600

    solve siderbar user photo hide issue and add a function to remove user from direct msg list

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 $(document).ready(function(){
     // selectors
-    $(".sidebar-top-item li ").click(toggleSubMenu)
+    $(".first-li").click(toggleSubMenu);
+    // $(".first-li ").click(function(){
+    //     $(".sidebar-sub-menu li ").off("click");
+    // });
+    // hide user 
+    $(".removeUser").click(function(){
+        console.log();
+        $(this.parentElement.parentElement.parentElement).hide();
+    })
 
 // toggle sub menu
 function toggleSubMenu(){
+    
     const subMenu = this?.children[1];
-
     if(subMenu){
      subMenu.classList.toggle("show");
     }
@@ -16,10 +24,11 @@ function toggleSubMenu(){
 function toggleIcon(targetElement){
     if(targetElement.children[0].children[0].children[0]){
        targetElement.children[0].children[0].children[0].classList.toggle("hide");
-       targetElement.children[0].children[0].children[1].classList.toggle("show");
+       targetElement.children[0]?.children[0]?.children[1]?.classList.toggle("show");
     }
     
 }
+
 
 
 //endline
